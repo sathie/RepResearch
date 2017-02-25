f21bff465345415aedd73cc09eb1a53d3f2dd323
Sarah Thiesen <sathiesen@gmail.com>
2017-02-25 22:50:44 +0100

    add R script

--- plotpractice.R
+++ plotpractice.R
@@ -1,19 +1,10 @@
----
-title: "Plotting Practice"
-author: "Sarah Thiesen"
-date: "February 25, 2017"
-output: pdf_document
----
-
-# Read data
-```{r}
+#Read data
 payments <- read.csv("/home/sarah/Uni/R/RepResearch/payments.csv")
-```
 
-#Question 1:
-Make a plot that answers the question: What is the relationship between mean covered charges (Average.Covered.Charges) and mean total payments (Average.Total.Payments) in New York?
+#Make a plot that answers the question: 
+#What is the relationship between mean covered charges (Average.Covered.Charges) and 
+#mean total payments (Average.Total.Payments) in New York?
 
-```{r, message = FALSE}
 require(dplyr)
 paymentsNY <- filter(payments, Provider.City == "NEW YORK")
 
@@ -21,27 +12,22 @@ require(ggplot2)
 p <- ggplot(paymentsNY, aes(Average.Covered.Charges, Average.Total.Payments))
 p + geom_point(alpha = 0.4, colour = "blue", size = 2) + 
   geom_smooth(method = lm, colour = "black", size = 0.5)
-```
 
-#Question 2:
-Make a plot (possibly multi-panel) that answers the question: How does the relationship between mean covered charges (Average.Covered.Charges) and mean total payments (Average.Total.Payments) vary by medical condition (DRG.Definition) and the state in which care was received (Provider.State)?
+#Make a plot (possibly multi-panel) that answers the question: 
+#How does the relationship between mean covered charges (Average.Covered.Charges) and 
+#mean total payments (Average.Total.Payments) vary by medical condition (DRG.Definition) 
+#and the state in which care was received (Provider.State)?
 
-```{r}
 q <- ggplot(payments, aes(log(Average.Covered.Charges), log(Average.Total.Payments)))
 q + geom_point(alpha = 0.1, colour = "blue", size = 0.5) + 
   facet_grid(Provider.State ~ DRG.Definition) +
   geom_smooth(method = lm, colour = "black", size = 0.5)
-```
-\newpage
-```{r, fig.height = 8}
+
 q + geom_point(aes(colour = DRG.Definition), alpha = 0.2) + 
   geom_smooth(aes(colour = DRG.Definition), method = lm, size = 0.5) + 
   facet_wrap(~Provider.State, nrow = 3) + 
   theme(legend.position = "bottom", legend.direction = "vertical")
-```
-\newpage
-```{r, fig.height = 8}
+
 q + geom_point(aes(colour = Provider.State), alpha = 0.2) + 
   geom_smooth(aes(colour = Provider.State), method = lm, size = 0.5) + 
   facet_wrap(~DRG.Definition, nrow = 3) + theme(legend.position = "bottom")
-```
